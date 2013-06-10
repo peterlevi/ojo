c7d8f6fdaec8ab6645aef83bdc462e1d24f56863
Peter Levi <peterlevi@peterlevi.com>
2013-06-11 01:03:25 +0300

    More functional browse mode

--- ojo/ojo.py
+++ ojo/ojo.py
@@ -12,7 +12,7 @@ class Ojo(Gtk.Window):
         self.screen = self.get_screen()
         self.set_position(Gtk.WindowPosition.CENTER)
         self.set_decorated(False)
-        self.maximize()
+        #self.maximize()
 
         self.visual = self.screen.get_rgba_visual()
         if self.visual and self.screen.is_composited():
@@ -21,7 +21,9 @@ class Ojo(Gtk.Window):
         self.connect("draw", self.area_draw)
 
         self.box = Gtk.VBox()
+        self.box.set_visible(True)
         self.image = Gtk.Image()
+        self.image.set_visible(True)
         self.box.add(self.image)
         self.add(self.box)
 
@@ -30,13 +32,22 @@ class Ojo(Gtk.Window):
                         Gdk.EventMask.SCROLL_MASK)
 
     def main(self):
-        self.show(os.path.normpath(sys.argv[1]))
-        self.show_all()
-        GObject.idle_add(self.after_quick_start)
+        path = os.path.realpath(sys.argv[1])
+        print path
+        if os.path.isfile(path):
+            self.show(path)
+            GObject.idle_add(self.after_quick_start)
+        else:
+            self.current = os.path.join(path, 'none')
+            self.after_quick_start()
+            self.show(self.images[0])
+            self.set_mode('folder')
+        self.set_visible(True)
+        self.show()
         Gtk.main()
 
     def area_draw(self, widget, cr):
-        if self.full:
+        if self.full and self.mode == "image":
             cr.set_source_rgba(0, 0, 0, 1.0)
         else:
             cr.set_source_rgba(77.0/255, 75.0/255, 69.0/255, 0.9)
@@ -49,6 +60,12 @@ class Ojo(Gtk.Window):
             print filename
         self.current = filename or self.current
         self.set_title(self.current)
+        if getattr(self, "web_view", None):
+            self.web_view.execute_script("select('%s')" % self.current)
+        if self.mode == 'image':
+            self.adjust_size()
+
+    def adjust_size(self):
         width = self.screen.get_width() if self.full else self.screen.get_width() - 100
         height = self.screen.get_height() if self.full else self.screen.get_height() - 200
         self.pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.current, width, height, True)
@@ -64,8 +81,7 @@ class Ojo(Gtk.Window):
             self.real_width = self.pixbuf.get_width() + 60
             self.real_height = self.pixbuf.get_height() + 60
             self.resize(self.real_width, self.real_height)
-            self.move(
-                (self.screen.get_width() - self.real_width) // 2,
+            self.move((self.screen.get_width() - self.real_width) // 2,
                 (self.screen.get_height() - self.real_height) // 2)
         else:
             self.box.set_margin_right(0)
@@ -91,16 +107,23 @@ class Ojo(Gtk.Window):
             full = not self.full
         self.full = full
         if self.full:
-            self.get_window().set_cursor(Gdk.Cursor.new_for_display(Gdk.Display.get_default(), Gdk.CursorType.BLANK_CURSOR))
             self.fullscreen()
         else:
-            self.get_window().set_cursor(Gdk.Cursor.new_for_display(Gdk.Display.get_default(), Gdk.CursorType.ARROW))
             self.unfullscreen()
+        self.update_cursor()
+
+    def update_cursor(self):
+        if self.get_window():
+            self.get_window().set_cursor(Gdk.Cursor.new_for_display(Gdk.Display.get_default(),
+                Gdk.CursorType.BLANK_CURSOR if self.full and self.mode == 'image' else Gdk.CursorType.ARROW))
 
     def set_mode(self, mode):
         self.mode = mode
+        if self.mode == 'image':
+            self.show()
         self.image.set_visible(self.mode == 'image')
         self.browser.set_visible(self.mode == 'folder')
+        self.update_cursor()
 
     def process_key(self, widget, event):
         key = Gdk.keyval_name(event.keyval)
@@ -109,7 +132,7 @@ class Ojo(Gtk.Window):
         elif key in ("f", "F", "F11"):
             self.toggle_fullscreen()
             self.show()
-        elif key in ("Right", "Left") and self.mode == "image":
+        elif key in ("Right", "Left"):
             GObject.idle_add(lambda: self.go(1 if key == "Right" else -1))
         elif key == 'Return':
             modes = ["image", "folder"]
@@ -157,7 +180,6 @@ class Ojo(Gtk.Window):
         self.connect("scroll-event", self.scrolled)
         self.folder = os.path.dirname(self.current)
         self.images = filter(self.is_image, map(lambda f: os.path.join(self.folder, f), sorted(os.listdir(self.folder))))
-        print self.images
 
         self.thumbs = {}
         GObject.idle_add(self.render_browser)
@@ -185,14 +207,13 @@ class Ojo(Gtk.Window):
             return True
         self.web_view.connect("navigation-policy-decision-requested", nav)
 
-        self.web_view.load_string(html, "text/html", "UTF-8", "file://")
+        self.web_view.connect('document-load-finished', lambda wf, data: self.prepare_thumbs()) # Load page
+        self.web_view.load_string(html, "text/html", "UTF-8", "file://" + os.path.dirname(__file__) + "/")
         self.web_view.set_visible(True)
         rgba = Gdk.RGBA()
         rgba.parse('rgba(0, 0, 0, 0)')
         self.web_view.override_background_color(Gtk.StateFlags.NORMAL, rgba)
         self.browser.add(self.web_view)
-
-        GObject.idle_add(self.prepare_thumbs)
 
     def prepare_thumbs(self):
         for img in self.images:
@@ -201,6 +222,8 @@ class Ojo(Gtk.Window):
                 try:
                     b64 = self.b64(img).replace('\n', '')
                     self.web_view.execute_script("add_image('%s', '%s', %s)" % (img, b64, 'true' if img==self.current else 'false'))
+                    if img == self.current:
+                        self.web_view.execute_script("select('%s')" % img)
                 except Exception, e:
                     print str(e)
                 GObject.idle_add(self.prepare_thumbs)
@@ -210,7 +233,7 @@ class Ojo(Gtk.Window):
         from PIL import Image
         import StringIO
         x = Image.open(img)
-        x.thumbnail((100000, 100))
+        x.thumbnail((500, 120))
         output = StringIO.StringIO()
         x.save(output, "PNG")
         contents = output.getvalue().encode("base64")
