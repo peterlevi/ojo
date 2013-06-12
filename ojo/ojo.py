45b5dbeb77d1e96d5c1275a6ce2a584f0538ed8f
Peter Levi <peterlevi@peterlevi.com>
2013-06-13 02:27:15 +0300

    Speed vs. orientation optimizations, other fixes

--- ojo/ojo.py
+++ ojo/ojo.py
@@ -17,11 +17,9 @@
 
 
 import cairo
-from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, Gio, WebKit
-from pyexiv2 import ImageMetadata
+from gi.repository import Gtk, Gdk, GdkPixbuf, GObject
 import os
 import sys
-import cStringIO
 
 class Ojo(Gtk.Window):
     def __init__(self):
@@ -52,6 +50,7 @@ class Ojo(Gtk.Window):
     def main(self):
         path = os.path.realpath(sys.argv[1])
         print "Started with: " + path
+        self.need_orientation = {}
         if os.path.isfile(path):
             self.mode = 'image'
             self.show(path)
@@ -61,14 +60,17 @@ class Ojo(Gtk.Window):
             self.current = os.path.join(path, 'none')
             self.after_quick_start()
             self.set_mode('folder')
-            self.show(self.images[0], True)
+            self.show(self.images[0])
 
         self.set_visible(True)
         Gtk.main()
 
     def area_draw(self, widget, cr):
         if self.full:# and self.mode == "image":
-            cr.set_source_rgba(0, 0, 0, 1.0)
+            if self.mode == 'folder':
+                cr.set_source_rgba(77.0/255, 75.0/255, 69.0/255, 1)
+            else:
+                cr.set_source_rgba(0, 0, 0, 1.0)
         else:
             cr.set_source_rgba(77.0/255, 75.0/255, 69.0/255, 0.9)
         cr.set_operator(cairo.OPERATOR_SOURCE)
@@ -78,24 +80,34 @@ class Ojo(Gtk.Window):
     def update_browser(self, file):
         self.web_view.execute_script("select('%s')" % file)
 
-    def show(self, filename=None, force_resize=False):
+    def show(self, filename=None, orient=False):
         if filename:
             print filename
-        self.current = filename or self.current
+        filename = filename or self.current
+        self.current = filename
+        self.selected = self.current
         self.set_title(self.current)
         if getattr(self, "web_view", None):
             self.update_browser(self.current)
-        if self.mode == 'image' or force_resize:
-            width = self.screen.get_width() if self.full else self.screen.get_width() - 150
-            height = self.screen.get_height() if self.full else self.screen.get_height() - 150
-            self.pixbuf = self.get_pixbuf(self.current, width, height)
-            self.image.set_from_pixbuf(self.pixbuf)
-            self.update_margins(force_resize)
+        width = self.screen.get_width() if self.full else self.screen.get_width() - 150
+        height = self.screen.get_height() if self.full else self.screen.get_height() - 150
+        self.pixbuf, oriented = self.get_pixbuf(self.current, width, height, orient)
+        self.image.set_from_pixbuf(self.pixbuf)
+        self.update_size()
 
-    def attempt_preview_data(self, img):
-        meta = ImageMetadata(img)
+        if not orient and not oriented:
+            def _check_orientation():
+                meta = self.get_meta(filename)
+                if self.current == filename and self.needs_orientation(meta):
+                    self.show(self.current, True)
+            GObject.idle_add(_check_orientation)
+
+    def get_meta(self, filename):
+        from pyexiv2 import ImageMetadata
+        meta = ImageMetadata(filename)
         meta.read()
-        return meta.previews[-1].data
+        self.need_orientation[filename] = self.needs_orientation(meta)
+        return meta
 
     def set_margins(self, margin):
         self.box.set_margin_right(margin)
@@ -103,7 +115,7 @@ class Ojo(Gtk.Window):
         self.box.set_margin_bottom(margin)
         self.box.set_margin_top(margin)
 
-    def update_margins(self, force_resize=False):
+    def update_size(self):
         if self.mode == 'folder':
             self.set_margins(15)
         elif self.full:
@@ -111,9 +123,18 @@ class Ojo(Gtk.Window):
         else:
             self.set_margins(30)
 
-        if self.mode == "image" or force_resize:
+        if self.mode == "image":
             self.real_width = self.pixbuf.get_width() + 60
             self.real_height = self.pixbuf.get_height() + 60
+        else:
+            self.real_width = self.screen.get_width() - 90
+            self.real_height = self.screen.get_height() - 90
+            if self.real_width > 1.5 * self.real_height:
+                self.real_width = int(1.5 * self.real_height)
+            else:
+                self.real_height = int(1.5 * self.real_width)
+
+        if not self.full:
             self.resize(self.real_width, self.real_height)
             self.move((self.screen.get_width() - self.real_width) // 2,
                 (self.screen.get_height() - self.real_height) // 2)
@@ -139,7 +160,7 @@ class Ojo(Gtk.Window):
         else:
             self.unfullscreen()
         self.update_cursor()
-        self.update_margins()
+        self.update_size()
 
     def update_cursor(self):
         if self.get_window():
@@ -148,12 +169,13 @@ class Ojo(Gtk.Window):
 
     def set_mode(self, mode):
         self.mode = mode
-        if self.mode == 'image':
-            self.show()
+        if self.mode == "image" and self.selected != self.current:
+            self.show(self.selected)
+        else:
+            self.update_size()
+        self.update_cursor()
         self.image.set_visible(self.mode == 'image')
         self.browser.set_visible(self.mode == 'folder')
-        self.update_cursor()
-        self.update_margins()
 
     def process_key(self, widget, event):
         key = Gdk.keyval_name(event.keyval)
@@ -163,6 +185,8 @@ class Ojo(Gtk.Window):
         elif key in ("f", "F", "F11"):
             self.toggle_fullscreen()
             self.show()
+        elif key == 'F5':
+            self.update_size()
         elif key == 'Return':
             modes = ["image", "folder"]
             self.set_mode(modes[(modes.index(self.mode) + 1) % len(modes)])
@@ -217,6 +241,8 @@ class Ojo(Gtk.Window):
         GObject.idle_add(self.render_browser)
 
     def render_browser(self):
+        from gi.repository import WebKit
+
         with open(os.path.join(os.path.dirname(os.path.normpath(__file__)), 'browse.html')) as f:
             html = f.read()
 
@@ -231,10 +257,10 @@ class Ojo(Gtk.Window):
             url = action.get_original_uri()
             if not url.startswith('ojo:') and not url.startswith('ojo-select:'):
                 return False
-            self.current = urllib.unquote(re.sub('^ojo.*:', '', url))
+            self.selected = urllib.unquote(re.sub('^ojo.*:', '', url))
             if url.startswith('ojo:'):
                 def _do():
-                    self.show()
+                    self.show(self.selected)
                     self.from_browser_time = time.time()
                     self.set_mode("image")
                 GObject.idle_add(_do)
@@ -269,9 +295,12 @@ class Ojo(Gtk.Window):
             position = 0
         if position < len(images):
             self.add_thumb(images[position])
-            GObject.idle_add(lambda: self.prepare_thumbs(position + 1, images))
+            def _next():
+                GObject.idle_add(lambda: self.prepare_thumbs(position + 1, images))
+            GObject.timeout_add(50, _next)
 
     def pixbuf_from_data(self, data, width, height):
+        from gi.repository import Gio
         input_str = Gio.MemoryInputStream.new_from_data(data, None)
         pixbuf = GdkPixbuf.Pixbuf.new_from_stream_at_scale(input_str, width, height, True, None)
         return pixbuf
@@ -279,19 +308,19 @@ class Ojo(Gtk.Window):
     def b64(self, filename, width, height):
         return self.pil_to_base64(self.get_pil(filename, width, height))
 
-    def get_pixbuf(self, filename, width, height):
-#        try:
-#            return GdkPixbuf.Pixbuf.new_from_file_at_scale(self.current, width, height, True)
-#        except GObject.GError:
-#            return self.pixbuf_from_data(self.attempt_preview_data(self.current), width, height)
-
-        return self.pil_to_pixbuf(self.get_pil(filename, width, height))
+    def get_pixbuf(self, filename, width, height, orient):
+        if not orient and (not filename in self.need_orientation or not self.need_orientation[filename]):
+            try:
+                oriented = filename in self.need_orientation and not self.need_orientation[filename]
+                return GdkPixbuf.Pixbuf.new_from_file_at_scale(self.current, width, height, True), oriented
+            except GObject.GError:
+                pass # below we'll use another method
+        return self.pil_to_pixbuf(self.get_pil(filename, width, height)), True
 
     def get_pil(self, filename, width, height):
         from PIL import Image
         import cStringIO
-        meta = ImageMetadata(filename)
-        meta.read()
+        meta = self.get_meta(filename)
         try:
             pil_image = Image.open(filename)
         except IOError:
@@ -301,6 +330,7 @@ class Ojo(Gtk.Window):
         return pil_image
 
     def pil_to_base64(self, pil_image):
+        import cStringIO
         output = cStringIO.StringIO()
         pil_image.save(output, "PNG")
         contents = output.getvalue().encode("base64")
@@ -308,6 +338,7 @@ class Ojo(Gtk.Window):
         return contents.replace('\n', '')
 
     def pil_to_pixbuf(self, pil_image):
+        import cStringIO
         if pil_image.mode != 'RGB':          # Fix IOError: cannot write mode P as PPM
             pil_image = pil_image.convert('RGB')
         buff = cStringIO.StringIO()
@@ -327,6 +358,12 @@ class Ojo(Gtk.Window):
 #        height, width = pil_image.size
 #        return GdkPixbuf.Pixbuf.new_from_data(
 #            arr, GdkPixbuf.Colorspace.RGB, True, 8, width, height, width * 4, None, None)
+
+    def needs_orientation(self, meta):
+        if 'Exif.Image.Orientation' in meta.keys():
+            return meta['Exif.Image.Orientation'].value != 1
+        else:
+            return False
 
     def auto_rotate(self, meta, im):
         from PIL import Image
