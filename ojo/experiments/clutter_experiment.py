41281e4097b30cb446842c439cdf988669308349
Peter Levi <peterlevi@peterlevi.com>
2013-07-04 19:09:08 +0300

    Experimenting with clutter - transparency, webview, etc.

--- ojo/experiments/clutter_experiment.py
+++ ojo/experiments/clutter_experiment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 #! -*- coding: utf-8 -*-
-from gi.repository import GObject, Gtk, GtkClutter, Clutter, GdkPixbuf, Cogl
+from gi.repository import GObject, Gtk, Gdk, GtkClutter, Clutter, GdkPixbuf, Cogl, WebKit
+import cairo
 import sys
 import os
 
@@ -16,39 +17,99 @@ def get_texture(img):
                         Clutter.TextureFlags.NONE)
     return t
 
-global current
-global t
-current = None
-t = None
+def make_transparent(widget):
+    rgba = Gdk.RGBA()
+    rgba.parse('rgba(0, 0, 0, 0)')
+    widget.override_background_color(Gtk.StateFlags.NORMAL, rgba)
 
-if __name__ == '__main__':
-    Clutter.init(sys.argv)
-    stage = Clutter.Stage()     # Create the Stage
-    stage.set_size(800, 600)
-    stage.set_title("Clutter tests")          # Window's title
-    stage.connect('destroy', lambda x: Clutter.main_quit())  # Connect signal 'destroy'
+def area_draw(widget, cr):
+    cr.set_source_rgba(77.0/255, 75.0/255, 69.0/255, 0.9)
+    cr.set_operator(cairo.OPERATOR_SOURCE)
+    cr.paint()
+    cr.set_operator(cairo.OPERATOR_OVER)
 
-    folder = '/media/data/Pics/Wallpapers/'
-    images = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith('.jpg')]
-    current = images[0]
+class Cl:
+    def main(self):
+    #    Clutter.init(sys.argv)
+        Gtk.init(sys.argv)
+        GtkClutter.init(sys.argv)
 
-    t = get_texture(current)
-    stage.add_actor(t)
-    stage.show_all()
+        window = Gtk.Window()
+        window.resize(800, 600)
+        window.set_title("Clutter tests window")          # Window's title
+        window.move(100, 100)
+        window.connect('destroy', lambda x: Gtk.main_quit())  # Connect signal 'destroy'
+        window.set_decorated(False)
 
-    def go(*args):
-        global current
-        global t
-        current = images[(images.index(current) + 1) % len(images)]
-        nextt = get_texture(current)
-        nextt.set_opacity(0)
-        stage.add_actor(nextt)
-        t.animatev(Clutter.AnimationMode.EASE_OUT_SINE, 300, ["opacity"], [0])
-        nextt.animatev(Clutter.AnimationMode.EASE_OUT_SINE, 300, ["opacity"], [255])
-        previoust = t
-        t = nextt
-        GObject.timeout_add(1000, lambda: previoust.destroy())
+        self.screen = window.get_screen()
 
-    stage.connect('button-press-event', go)
+        self.visual = self.screen.get_rgba_visual()
+        if self.visual and self.screen.is_composited():
+            window.set_visual(self.visual)
+        window.set_app_paintable(True)
+        window.connect("draw", area_draw)
 
-    Clutter.main()                   # Start the application
+        box = Gtk.VBox()
+        box.set_margin_top(30)
+        box.set_margin_bottom(30)
+        box.set_margin_left(30)
+        box.set_margin_right(30)
+        make_transparent(box)
+        window.add(box)
+
+        embed = GtkClutter.Embed()
+        make_transparent(embed)
+        box.add(embed)
+
+        stage = embed.get_stage()
+    #    stage = Clutter.Stage()     # Create the Stage
+        color = Clutter.Color.new(77, 75, 69, 0.9 * 255)
+        stage.set_color(color)
+        #stage.set_opacity(128)
+        stage.set_use_alpha(True)
+        stage.set_size(800, 600)
+        stage.set_title("Clutter tests stage")          # Window's title
+        #stage.set_fullscreen(True)
+
+        layout = Clutter.BinLayout()
+        stage.set_layout_manager(layout)
+
+        folder = '/media/data/Pics/Wallpapers/Favorites/'
+        images = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith('.jpg')]
+        self.current = images[0]
+
+        self.current_texture = get_texture(self.current)
+        stage.add_actor(self.current_texture)
+
+        web_view = WebKit.WebView()
+        web_view.set_transparent(True)
+        make_transparent(web_view)
+        web_view.set_can_focus(True)
+        web_view.load_string("<html><body style='background: rgba(0, 0, 0, 0)'>AAAAAAAAAAA</body></html>", "text/html", "UTF-8", "file://" + os.path.dirname(__file__) + "/")
+        web_view_actor = GtkClutter.Actor.new_with_contents(web_view)
+        layout.add(web_view_actor, Clutter.BinAlignment.CENTER, Clutter.BinAlignment.END)
+
+        #    stage.show_all()
+        window.show_all()
+
+        def go(*args):
+            self.current = images[(images.index(self.current) + 1) % len(images)]
+            nextt = get_texture(self.current)
+            nextt.set_opacity(0)
+            stage.add_actor(nextt)
+            nextt.set_x((stage.get_width() - nextt.get_width()) / 2)
+            nextt.set_y((stage.get_height() - nextt.get_height()) / 2)
+            def a():
+                self.current_texture.animatev(Clutter.AnimationMode.EASE_OUT_SINE, 500, ["opacity"], [0])
+                nextt.animatev(Clutter.AnimationMode.EASE_OUT_SINE, 500, ["opacity"], [255])
+                previoust = self.current_texture
+                self.current_texture = nextt
+                GObject.timeout_add(1000, lambda: previoust.destroy())
+            GObject.idle_add(a)
+
+        stage.connect('button-press-event', go)
+
+    #    Clutter.main()                   # Start the application
+        Gtk.main()                   # Start the application
+
+Cl().main()
