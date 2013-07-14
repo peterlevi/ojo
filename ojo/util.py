f368681b84c821a77a8f201daebdeda0b3d0e0fc
Peter Levi <peterlevi@peterlevi.com>
2013-07-14 21:27:09 +0300

    Util: option to get icon path by name

--- ojo/util.py
+++ ojo/util.py
@@ -13,9 +13,13 @@ def get_folder_icon(path, size):
     from gi.repository import Gtk
     name = get_folder_icon_name(path)
     try:
-        return Gtk.IconTheme.get_default().lookup_icon(name, size, 0).get_filename()
+        return get_icon_path(name, size)
     except Exception:
-        return Gtk.IconTheme.get_default().lookup_icon('folder', size, 0).get_filename()
+        return get_icon_path('folder', size)
+
+def get_icon_path(icon_name, size):
+    from gi.repository import Gtk
+    return Gtk.IconTheme.get_default().lookup_icon(icon_name, size, 0).get_filename()
 
 if __name__ == "__main__":
     print get_folder_icon('/', 24)
